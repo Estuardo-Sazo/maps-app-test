4d92ddc76a9a7f92d93aa6f162f87de510a6b464
Estuardo-Sazo <estuardosazo1206@gmail.com>
2024-05-21 19:24:43 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:maps_app1/screens/screens.dart';
+
+void main() => runApp(const MapsApp());
+
+class MapsApp extends StatelessWidget {
+  const MapsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MapsApp',
+      home: LoadingScreen(),
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:maps_app1/screens/gps_access_screen.dart';
+export 'package:maps_app1/screens/map_screen.dart';
+export 'package:maps_app1/screens/loading_screen.dart';
