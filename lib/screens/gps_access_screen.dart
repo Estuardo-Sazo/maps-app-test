a23b0a9c18e1732c1159508455bd382b02f74c55
Estuardo-Sazo <estuardosazo1206@gmail.com>
2024-05-21 21:22:20 -0600

    Pantalla de permiso de GPS

--- lib/screens/gps_access_screen.dart
+++ lib/screens/gps_access_screen.dart
@@ -7,8 +7,46 @@ class GpsAccessScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('GPS Access Screen'),
+        child: _AccessButton(),
       ),
+    );
+  }
+}
+
+class _AccessButton extends StatelessWidget {
+  const _AccessButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Es necesario el acceso al GPS'),
+        MaterialButton(
+          onPressed: () {},
+          color: Colors.black,
+          shape: const StadiumBorder(),
+          elevation: 0,
+          child: const Text('Solicitar Acceso',
+              style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    );
+  }
+}
+
+class _EnableGpsMessage extends StatelessWidget {
+  const _EnableGpsMessage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Debe habilitar el Gps para usar esta App',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
     );
   }
 }
